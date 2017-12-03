1ac066080ae3aeae72f9a37fae2163145022ef0b
frkwtf <frkwtf@gmail.com>
2017-12-03 11:06:37 +0100

    Practica2

--- Diaz_Gil_Manuel/p2/DefensesSelection.cpp
+++ Diaz_Gil_Manuel/p2/DefensesSelection.cpp
@@ -10,64 +10,67 @@
 
 using namespace Asedio;
 
-float valorar(const Defense& d){
-	std::cout<<"Radio: "<<d.range<<" dispersion: "<<d.dispersion<<" Daño: "<<d.damage<<" ataques por segundo: "<<d.attacksPerSecond<<" vida: "<<d.health <<"coste: " <<d.cost<<std::endl;
-	return (d.range/30)*2+d.dispersion+((d.damage*d.attacksPerSecond)/5)*4+(d.health/500)*2+(1-(d.cost/105)*10);
+double valorar(const Defense& d){
+    double x= (d.range/30)*10+d.dispersion*12+((d.damage*d.attacksPerSecond)/5)*15+(d.health/500)*5+(1-(d.cost/150))*8;
+    std::cout<<x<<std::endl;
+    return x;
+
 }
 
 void DEF_LIB_EXPORTED selectDefenses(std::list<Defense*> defenses, unsigned int ases, std::list<int> &selectedIDs
             , float mapWidth, float mapHeight, std::list<Object*> obstacles) {
-	float tabla[defenses.size()][ases];
-	float valor[defenses.size()];
-	int costes[defenses.size()];
+    double tabla[defenses.size()][ases];
+    double valor[defenses.size()];
+    double costes[defenses.size()];
 
 
     std::list<Defense*>::iterator it = defenses.begin();
     int i = 0;
     for(it = defenses.begin(); it!=defenses.end();++it){
-    	valor[i]=valorar(*(*it));
-    	costes[i]=(*it)->cost;
+        valor[i] = valorar(*(*it));
+        costes[i]=(*it)->cost;
+        i++;
     }
 
     for (int j = 0; j < ases; ++j)
     {
-    	if(j<costes[0]){
-    		tabla[0][j] = 0;
-    	}else{
-    		tabla[0][j] = valor[0];
-    	}
+        if(j<costes[0]){
+            tabla[0][j] = 0;
+        }else{
+            tabla[0][j] = valor[0];
+        }
     }
 
-    for (int j = 1; j < defenses.size(); ++j)
+    for (i = 1; i < defenses.size(); i++)
     {
-    	for (int z = 0; z < ases; ++z)
-    	{
-    		if(z < costes[j]){
-    			tabla[j][z] = tabla[j-1][z];
-    		}else{
-    			tabla[j][z] = std::max(tabla[j-1][z],(tabla[j-1][z-costes[j]]+valor[i]));
-    		}
-    		
-    	}
-    	
+        for (int j = 0; j < ases; j++)
+        {
+            if(j < costes[i]){
+                tabla[i][j] = tabla[i-1][j];
+            }else{
+                tabla[i][j] = std::max(tabla[i-1][j],tabla[i-1][j-(int)costes[i]]+valor[i]);
+            }
+            
+        }
+        
     }
 
 
     
     it = defenses.begin();
-    unsigned int currentases = ases;
+    int currentases = ases;
     selectedIDs.push_back((*it)->id);
-    currentases -= (*it)->cost;
+    currentases -= (*it)->cost - 1;
 
     it = defenses.end();
     for (i = defenses.size()-1; i > 0; --i)
     {
-    	--it;
-    	if (tabla[i][currentases]!=tabla[i-1][currentases])
-    	{
-    		selectedIDs.push_back((*it)->id);
-    		currentases -= (*it)->cost;
-    	}
+        --it;
+        if (tabla[i][currentases]!=tabla[i-1][currentases])
+        {
+            selectedIDs.push_back((*it)->id);
+            currentases -= (*it)->cost;
+        }
     }
 
     /**
